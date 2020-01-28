55330401238fd2d5e2c6aff46a0e7e206da7e98a
DorukanE <dorukaneskiceri@gmail.com>
2020-01-29 02:50:45 +0300

    'list directors and films' feature added

--- routes/directors.js
+++ routes/directors.js
@@ -15,4 +15,49 @@ router.post('/', (req, res, next) => {
     });
 });
 
+router.get('/', (req, res) =>{
+  Director.aggregate([
+    {
+      $lookup: {
+        from: 'movies',
+        localField: '_id',
+        foreignField: 'director_id',
+        as: 'Movies'
+      }
+    },
+    {
+      $unwind: {
+        path: '$Movies',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        movies: {
+          $push: '$Movies'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        movies: '$movies'
+      }
+    }
+  ], (err, data) =>{
+    if(err)
+      res.json(err);
+
+    res.json(data);
+  });
+});
+
 module.exports = router;
