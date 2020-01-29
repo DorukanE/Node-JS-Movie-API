f738c1247bcef9e9bab16c1e894e6690f5aa8762
DorukanE <dorukaneskiceri@gmail.com>
2020-01-29 03:45:51 +0300

    validations updated

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,30 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
     title: {
         type: String,
-        required: true
+        required: [true, '{PATH} alanı zorunludur.'],
+        maxlength: [25, '{PATH} alanına maksimum {MAXLENGTH} değer girebilirsiniz.'],
+        minlength: [1, '{PATH} alanına minimum {MINLENGTH} değer girebilirsiniz.']
     },
-    category: String,
-    country: String,
-    year: Number,
-    imdb_score: Number,
+    category: {
+        type: String,
+        maxlength: [30, '{PATH} alanına maksimum {MAXLENGTH} değer girebilirsiniz.'],
+        minlength: [1, '{PATH} alanına minimum {MINLENGTH} değer girebilirsiniz.']
+    },
+    country: {
+        type: String,
+        maxlength: [25, '{PATH} alanına maksimum {MAXLENGTH} değer girebilirsiniz.'],
+        minlength: [1, '{PATH} alanına minimum {MINLENGTH} değer girebilirsiniz.']
+    },
+    year: {
+        type: Number,
+        max: 2020,
+        min: 1910
+    },
+    imdb_score: {
+        type: Number,
+        max: 10,
+        min: 0
+    },
     createdAt: {
         type: Date,
         default: Date.now
