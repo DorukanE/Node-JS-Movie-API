4d68691eedb8ba7648f6d3c34fb3653bd90bc8ea
DorukanE <dorukaneskiceri@gmail.com>
2020-01-29 22:45:19 +0300

    'create director' test added

---
+++ test/api/director-test.js
@@ -0,0 +1,42 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../../app');
+
+chai.use(chaiHttp);
+
+let token;
+describe('/api/directors test', () =>{
+    before((done) =>{
+        chai.request(server)
+            .post('/authenticate')
+            .send({username: 'doruk', password: '12345'})
+            .end((err, res) =>{
+                token = res.body.token
+                done();
+            });
+    });
+
+    describe('POST add director', () =>{
+        it('(POST /) director test', (done) =>{
+            const directors = {
+                name: 'ahmetcan',
+                surname: 'varlı',
+                bio: 'lorem ipsum'
+            }
+            chai.request(server)
+                .post('/api/directors')
+                .send(directors)
+                .set('x-access-token', token)
+                .end((err, res) =>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('name').eql(directors.name);
+                    res.body.should.have.property('surname').eql(directors.surname);
+                    res.body.should.have.property('bio').eql(directors.bio);
+                    done();
+                });
+        });
+    });
+});
+
