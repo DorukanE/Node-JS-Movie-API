82679b77709bec3e416c4d3f843b3eb6bb3e215d
DorukanE <dorukaneskiceri@gmail.com>
2020-01-29 21:37:19 +0300

    'list movie by id' test added

--- test/api/movies-test.js
+++ test/api/movies-test.js
@@ -5,7 +5,7 @@ const server = require('../../app');
 
 chai.use(chaiHttp);
 
-let token;
+let token, movieId;
 describe('/api/movies tests', () =>{
     before((done) =>{
         chai.request(server)
@@ -52,6 +52,28 @@ describe('/api/movies tests', () =>{
                     res.body.should.have.property('country');
                     res.body.should.have.property('year');
                     res.body.should.have.property('imdb_score');
+                    movieId = res.body._id;
+                    done();
+                });
+        });
+    });
+
+    describe('List movie', () =>{
+        it('(GET /) list movie by Id', (done) =>{
+            chai.request(server)
+                .get('/api/movies/' + movieId)
+                .send(movieId)
+                .set('x-access-token', token)
+                .end((err, res) =>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('title');
+                    res.body.should.have.property('director_id');
+                    res.body.should.have.property('category');
+                    res.body.should.have.property('country');
+                    res.body.should.have.property('year');
+                    res.body.should.have.property('imdb_score');
+                    res.body.should.have.property('_id').eql(movieId);
                     done();
                 });
         });
