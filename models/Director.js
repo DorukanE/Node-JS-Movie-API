2d61f179ba6febae9da35f1fd102a7f579b0a6ff
DorukanE <dorukaneskiceri@gmail.com>
2020-01-29 03:48:32 +0300

    validations updated

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        max: 60,
+        min: 1
+    },
+    surname: {
+        type: String,
+        max: 60,
+        min: 1
+    },
+    bio: {
+        type: String,
+        max: 1000,
+        min: 1
+    },
     createdAt: {
         type: Date,
         default: Date.now
